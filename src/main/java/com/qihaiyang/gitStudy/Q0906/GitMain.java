ce6553095e0e00592b6d9d82715537cc6989d909
qihaiyang <qihaiyang@qq.com>
2023-09-06 10:37:28 +0800

    second Commit

--- src/main/java/com/qihaiyang/gitStudy/Q0906/GitMain.java
+++ src/main/java/com/qihaiyang/gitStudy/Q0906/GitMain.java
@@ -3,6 +3,7 @@ package com.qihaiyang.gitStudy.Q0906;
 public class GitMain {
     public static void main(String[] args){
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 
 }
